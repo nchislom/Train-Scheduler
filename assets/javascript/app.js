861bd3398dc63b81ee213f9bf99867bb9e7ec5d6
NChislom <11775893+nchislom@users.noreply.github.com>
2019-04-22 08:58:29 -0400

    Initial commit. Scaffolding complete

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDN2jb9EjvwpXcO6oLGdEpBZLurDoevBFQ",
+    authDomain: "test-d6eee.firebaseapp.com",
+    databaseURL: "https://test-d6eee.firebaseio.com",
+    projectId: "test-d6eee",
+    storageBucket: "test-d6eee.appspot.com",
+    messagingSenderId: "580661366404"
+};
+firebase.initializeApp(config);
